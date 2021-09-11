b8c35d2e70de02fffd4b0d4fac6e4828d74bd4eb
G1redE <90474166+G1redE@users.noreply.github.com>
2021-09-11 22:23:59 +0530

    ifelse and terinary

---
+++ Ifelse.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Ifelse {
+	public static void main(String[] args) {
+		int x =50,y=40;
+		if(x>y) {
+			System.out.println("The greatest integer is " + x);
+		}else {
+			System.out.println("The greatest integer is" + y);
+		}
+	}
+	}
---
+++ terinary.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Terinary {
+	public static void main(String[] args) {
+	int x =50,y=40 ;
+	String op = "The greatest integer is ";
+	op += (x>y)?x:y;
+	System.out.println(op);
+}
+}
