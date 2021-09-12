9504ae6ed4ea82848b2edce50370a6465db285b9
G1redE <90474166+G1redE@users.noreply.github.com>
2021-09-12 23:24:43 +0530

    12/09/2021

--- LargestNnumbers.java
+++ LargestNnumbers.java
@@ -2,9 +2,9 @@ package Test;
 
 public class LargestNnumbers {
 	public static void main(String[] args) {
-	    int[] arr = {3,20,1,14,5,6,4};
-	    int largest = 0;
-	    for(int i=0;i<arr.length;i++) {
+	    int[] arr = {-100,-24,1,0,50,6,-4};
+	    int largest = arr[0];
+	    for(int i=1;i<arr.length;i++) {
 	    	if(arr[i]>largest) {
 		    	largest=arr[i];
 		    }
